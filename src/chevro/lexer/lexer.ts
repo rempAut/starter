2a00a75488e119a30de01b9af4fe98f381896cb9
Jidost <jiridost@gmail.com>
2021-10-26 12:02:32 +0200

    runnable chevrotain

---
+++ src/chevro/lexer/lexer.ts
@@ -0,0 +1,46 @@
+// Implementation of A lexer for IEC 61131-3 Structured text grammar
+import { createToken, Lexer, TokenType } from 'chevrotain';
+
+// the vocabulary will be exported and used in the Parser definition.
+export const tokenVocabulary: { [x: string]: chevrotain.TokenType; } = {}
+
+// specify tokens
+const Identifier = createToken({ name: "Identifier", pattern: /[a-zA-Z_][\.\w\d_]*/ });
+const Float = createToken({ name: "Float", pattern: /\d+\.\d+/ });
+const Integer = createToken({ name: "Integer", pattern: /\d+/, longer_alt: Float });
+
+// skipped token
+const WhiteSpace = createToken({
+  name: "WhiteSpace",
+  pattern: /\s+/,
+  group: Lexer.SKIPPED
+})
+
+// note we are placing WhiteSpace first as it is very common thus it will speed up the lexer.
+let allTokens = [
+  WhiteSpace,
+  // "keywords" appear before the Identifier
+  // The Identifier must appear after the keywords because all keywords are valid identifiers.
+  Identifier,
+  Integer,
+]
+
+const SelectLexer = new Lexer(allTokens)
+
+
+allTokens.forEach((tokenType) => {
+  tokenVocabulary[tokenType.name] = tokenType
+})
+
+
+export function lex(inputText: string) {
+
+  return "hello from lexer"
+  const lexingResult = SelectLexer.tokenize(inputText)
+
+  if (lexingResult.errors.length > 0) {
+    throw Error("Sad Sad Panda, lexing errors detected")
+  }
+
+  return lexingResult
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { helloWorld } from './app/HelloWorld';
+import { lex } from './chevro/lexer/lexer';
+
 
 console.log(helloWorld());
+
+
+const inputText = "SELECT column1 FROM table2"
+const lexingResult = lex(inputText)
+console.log(JSON.stringify(lexingResult, null, "\t"))
